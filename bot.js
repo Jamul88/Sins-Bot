bde9522d0235f6d6c4607ac5ff361ab85e11a89c
Jamul88 <39919368+Jamul88@users.noreply.github.com>
2018-06-05 15:33:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -146,14 +146,12 @@ client.on("message", async message => {
   if (command === "mok") { 
     return message.channel.send("Your Anis is very good nahuy");
   }
-  if (command === "johnny") { 
-    return message.channel.send("Subscribe my youtube Channel SinsTV");
-  }
   if (command === "sins") { 
     return message.channel.send("Subscribe my youtube Channel SinsTV");
   }
   else if(command === "help") {
-   message.author.sendMessage("lel, ROFL"); return;
+   message.author.sendMessage("You want to know what Sins-Bot can do? Then you're in the wrong website here you can see it 
+                              https://de.pornhub.com/pornstar/johnny-sins "); return;
   } 
  else {
     message.channel.send("You have a tiny Cock");
