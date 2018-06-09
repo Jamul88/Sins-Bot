ed142604c61f0eb62827973547653baebfd7388a
Jamul88 <39919368+Jamul88@users.noreply.github.com>
2018-06-09 20:06:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -128,8 +128,8 @@ client.on("message", async message => {
   if (command === "sins") { 
     return message.channel.send("Subscribe my youtube Channel SinsTV");
   }
-  if (command === "bj") { 
-    return message.channel.send("https://de.pornhub.com/photo/84956551");
+  if (command === "gay") { 
+    return message.channel.send("https://www.instagram.com/p/Bd7vQS9H2vE/?taken-by=alles__nur__memes i am not gay");
   }
   else if(command === "help") {
    message.author.send.Message("You want to know what Sins-Bot can do? Then you're in the wrong website here you can see it https://de.pornhub.com/pornstar/johnny-sins "); return;
