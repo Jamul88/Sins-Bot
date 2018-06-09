d04ea877a6ede1a958d5bb04d8afd5308dd42c03
Jamul88 <39919368+Jamul88@users.noreply.github.com>
2018-06-09 19:27:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -132,7 +132,7 @@ client.on("message", async message => {
     return message.channel.send("https://de.pornhub.com");
   }
   else if(command === "help") {
-   message.author.sendMessage("You want to know what Sins-Bot can do? Then you're in the wrong website here you can see it https://de.pornhub.com/pornstar/johnny-sins "); return;
+   message.author.send.Message("You want to know what Sins-Bot can do? Then you're in the wrong website here you can see it https://de.pornhub.com/pornstar/johnny-sins "); return;
   } 
  else {
     message.channel.send("You have a tiny Cock");
